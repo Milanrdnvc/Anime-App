c5d76f3b81139aa467f21c9471ac01b1dd685fd6
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-08 16:08:52 +0200

    added info to modal

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ const search = document.querySelector('input');
 const modal = document.querySelector('.modal');
 const blurBody = document.querySelector('.blur-body');
 const modalClose = document.querySelector('.modal__close-btn');
+const modalTitle = document.querySelector('.modal__title');
+const modalInfo = document.querySelector('.modal__info ul');
+const modalDesc = document.querySelector('.modal__desc');
+const modalLink = document.querySelector('.modal__link');
 let err;
 let anime;
 
-search.addEventListener('click', () => {
+modalClose.addEventListener('click', hideModal)
+
+function showModal() {
     modal.classList.add('active');
     blurBody.classList.add('active');
     imgGrid.style.zIndex = '-2';
-});
+}
 
-modalClose.addEventListener('click', () => {
+function hideModal() {
     modal.classList.remove('active');
     blurBody.classList.remove('active');
-    imgGrid.style.zIndex = '0';
-});
+    imgGrid.style.zIndex = '0';  
+}
 
 function fetchAnime(api) {
     fetch(api)
@@ -40,6 +46,18 @@ function renderDOM(response, err) {
         let img = document.createElement('img');
         img.src = item.image_url;
         imgGrid.appendChild(img);
+        img.addEventListener('click', () => {
+            showModal();
+            modalTitle.innerText = item.title;
+            let info = modalInfo.querySelectorAll('li');
+            info[0].innerText = `Episodes: ${item.episodes}`;
+            info[1].innerText = `Rated: ${item.rated}`;
+            info[2].innerText = `Score: ${item.score}`;
+            modalDesc.innerText = item.synopsis;
+            let readMore = modalLink.querySelector('a');
+            readMore.href = item.url;
+            readMore.target = '_blank';
+        });
     });
 }
 
